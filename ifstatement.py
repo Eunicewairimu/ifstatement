b00b50d8098f45bf88557cb113745f8c52256a93
Eunice wairimu <eunicewairimu227@gmail.com>
2018-06-09 14:02:59 +0300

    ifstatement

---
+++ ifstatement.py
@@ -0,0 +1,25 @@
+marks=80
+if marks==60:
+	print("grade is B")
+elif marks>=60 and marks<70:
+	print("grade is C")
+elif marks>=70 and marks<80:
+	print("grade is A")
+elif marks>=30 and marks<40:
+	print("grade is D")
+else:
+	print("grade is B")
+
+x = int(input(" 5: "))
+if x < 5:
+   x = 5
+   print('Negative changed to five')
+elif x == 5:
+    print('5')
+elif x == 6:
+    print('6')
+else:
+    print('More')
+
+
+	
